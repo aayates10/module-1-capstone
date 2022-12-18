291d32d723f2591be7f817e40c84eb07c4758575
Biggs <jwbiggs069@gmail.com>
2022-12-18 13:05:32 -0600

    Changed the math for coinsReturned. Fixed rounding error.

--- src/main/java/com/techelevator/Transaction.java
+++ src/main/java/com/techelevator/Transaction.java
@@ -4,28 +4,22 @@ import java.awt.*;
 import java.util.Arrays;
 
 public class Transaction {
-    private static final double NICKEL = 0.05;
-    private static final double DIME = 0.10;
-    private static final double QUARTER = 0.25;
+    private static final int NICKEL = 5;
+    private static final int DIME = 10;
+    private static final int QUARTER = 25;
 
     public static String coinsReturned(double changeNeeded) {
         // changeNeeded needs to be a provided variable.
-        double changeGiven = 0;
+        int changeGiven = (int ) Math.ceil(changeNeeded * 100);
         int quartersGiven = 0;
         int dimesGiven = 0;
         int nickelsGiven = 0;
-        while ((changeNeeded - changeGiven) >= QUARTER) {
-            changeGiven = changeGiven + QUARTER;
-            quartersGiven++;
-        }
-        while ((changeNeeded - changeGiven) >= DIME) {
-            changeGiven = changeGiven + DIME;
-            dimesGiven++;
-        }
-       while ((changeNeeded - changeGiven) >= NICKEL) {
-            changeGiven = changeGiven + NICKEL;
-            nickelsGiven++;
-        }
+        quartersGiven = (changeGiven / QUARTER);
+        changeGiven %= QUARTER;
+        dimesGiven = (changeGiven / DIME);
+        changeGiven %= DIME;
+        nickelsGiven = (changeGiven / NICKEL);
+        changeGiven %= NICKEL;
         return "Dispensing change in the form of " + quartersGiven + " quarters, " + dimesGiven + " dimes, and " + nickelsGiven + " nickels";
     }
 }
